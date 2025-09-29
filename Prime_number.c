e2d7e52d04cba5aa737f10124b1d9adfbe2119bb
sivapriya <sivapriyasanda123@gmail.com>
2025-09-29 11:52:42 +0530

    Initial commit

---
+++ Prime_number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int num,isprime=1;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num <= 1)
+    {
+        isprime=0;
+    }
+    else
+    {
+        for(int i=2;i<=num/2;i++)
+        {
+            if(num % i == 0)
+            {
+                isprime = 0;
+                break;
+            }
+        }
+    }
+
+    if(isprime)
+    {
+        printf("%d is a Prime number.\n",num);
+
+    }
+    else
+    {
+        printf("%d is Not a Prime number.\n",num);
+    }
+}
